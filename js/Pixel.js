d15bab0a69ac5c91c2ed6a1bb8865140df3a31b9
Laurent Dufour <laurent.dufour@itkweb.com>
2014-06-11 10:41:07 +0200

    pixel move

--- js/Pixel.js
+++ js/Pixel.js
@@ -1,8 +1,23 @@
 function Pixel() {
 	this.htmlElement = document.createElement('div');
+	this.htmlElement.id = "superpixel";
 	this.htmlElement.classList.add('pixel');
 };
 
+
+
 Pixel.prototype.start = function start(map) {
-  map.addChild(this.htmlElement);
+	var that = this;  
+  	map.addChild(this.htmlElement);
+
+	window.onmousemove = function(e){
+	    that.MovePixelAss(e.clientX,e.clientY);
+	}
+
 };
+
+
+	Pixel.prototype.MovePixelAss = function move(x,y) {
+		this.htmlElement.style.left = x+"px";
+		this.htmlElement.style.top = y+"px";
+	}
